d3187f4cc2442be234f54f29ddeb0a0bf3a319d8
Giacomo Capodaglio <gcapodaglio@gr-fe1.lanl.gov>
2020-10-19 23:14:34 -0600

    modified build_base_mesh.py to do a variable res mesh

--- testing_and_setup/compass/ocean/aquaplanet/variable_res/default/build_base_mesh.py
+++ testing_and_setup/compass/ocean/aquaplanet/variable_res/default/build_base_mesh.py
@@ -25,15 +25,16 @@ def cellWidthVsLatLon():
     """
     createPlots = True
     ddeg = 10.0 # computed grid resolution, in degrees
-    latCenter = 45.0 # center point in degrees
-    lonCenter = 0.0 # center point in degrees
+    latCenter = 90.0 # center point in degrees
+    lonCenter = 135.0 # center point in degrees
 
-# Giacomo, after we get this working with an earth-sized sphere, we need to
-# change these flags to be a unit sphere, or reduce it to a unit sphere at the
-# end of this routine
-    courseResolution = 240 # km
+    # Giacomo, after we get this working with an earth-sized sphere, we need to
+    # change these flags to be a unit sphere, or reduce it to a unit sphere at the
+    # end of this routine
+    coarseResolution = 240 # km
+    fineResolution = 140 # km
     refinementFactor = 2
-    fineRadius = 1000 # km
+    fineRadius = 5000 # km
     transitionWidth = 100 # km
     earthRadius = 6.371e3 # radius in km
     
@@ -46,16 +47,12 @@ def cellWidthVsLatLon():
     lonGrid, latGrid = np.meshgrid(np.deg2rad(lon), np.deg2rad(lat))
 
     # Halversine formula for distance
-    # better to compute it in code directly, but example functions are below
-# Giacomo, I just got this formula from this post:
-# https://stackoverflow.com/a/51722117/12656503
-# please double check it against an original formula.
-    distance = earthRadius * np.sin((latGrid - latCenter)/2)**2 \
-        + np.cos(latCenter)*np.cos(latGrid) * np.sin((lonGrid - lonCenter)/2)**2
+    distance =  np.sin((latGrid - latCenter)*0.5)**2 + np.cos(latCenter)*np.cos(latGrid) * np.sin((lonGrid - lonCenter)*0.5)**2
+    distance = 2.0 * earthRadius * np.arctan2(np.sqrt(distance),np.sqrt(1.0 - distance)) 
+
+    tanhDistance = np.tanh((fineRadius-distance)/transitionWidth)
      
-    tanhDistance = np.tanh(distance)
-    # Next line is just a dummy line, fill in with actual cell width.
-    cellWidth = courseResolution*np.ones(np.shape(distance))
+    cellWidth = coarseResolution + (fineResolution - coarseResolution) * tanhDistance
 
     if createPlots:
         varList = ['latGrid','lonGrid','distance','tanhDistance','cellWidth']
@@ -73,52 +70,6 @@ def cellWidthVsLatLon():
             plt.colorbar()
         plt.savefig('cellWidth.png')
     return cellWidth, lon, lat
-
-
-# code examples from
-# https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-# this fuction can be removed later if not used.
-def distance(s_lat, s_lng, e_lat, e_lng):
-
-   # approximate radius of earth in km
-   R = 6373.0
-
-   s_lat = s_lat*np.pi/180.0                      
-   s_lng = np.deg2rad(s_lng)     
-   e_lat = np.deg2rad(e_lat)                       
-   e_lng = np.deg2rad(e_lng)  
-
-   d = np.sin((e_lat - s_lat)/2)**2 + np.cos(s_lat)*np.cos(e_lat) * np.sin((e_lng - s_lng)/2)**2
-
-   return 2 * R * np.arcsin(np.sqrt(d))
-
-# code examples from
-# https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-# this fuction can be removed later if not used.
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-
-    #Convert decimal degrees to Radians:
-    lon1 = np.radians(lon1.values)
-    lat1 = np.radians(lat1.values)
-    lon2 = np.radians(lon2.values)
-    lat2 = np.radians(lat2.values)
-
-    #Implementing Haversine Formula: 
-    dlon = np.subtract(lon2, lon1)
-    dlat = np.subtract(lat2, lat1)
-
-    a = np.add(np.power(np.sin(np.divide(dlat, 2)), 2),  
-            np.multiply(np.cos(lat1), 
-            np.multiply(np.cos(lat2), 
-            np.power(np.sin(np.divide(dlon, 2)), 2))))
-    c = np.multiply(2, np.arcsin(np.sqrt(a)))
-    r = 6371
-
-    return c*r
 
 def main():
     cellWidth, lon, lat = cellWidthVsLatLon()
